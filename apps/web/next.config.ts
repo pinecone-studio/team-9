c189b1374a9ee311f3ba67a0aaf9c6184d82fd7a
25lp3051 <25lp3051@setups-iMac-2.local>
2026-03-12 15:16:10 +0800

    next config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_GRAPHQL_ENDPOINT: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+  },
   output: "export",
   reactCompiler: true,
 };
